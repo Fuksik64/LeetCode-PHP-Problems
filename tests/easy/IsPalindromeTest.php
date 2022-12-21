6a5aad5c898054fcb103087a33546488db0b55d1
Fuksik64 <61504720+Fuksik64@users.noreply.github.com>
2022-12-21 09:09:32 +0100

    solve is palindrome problem

---
+++ tests/easy/IsPalindromeTest.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+ --------------------------------------------------------------------------
+    Given an integer x, return true if x is a palindrome, and false otherwise.
+
+    Palindrome: An integer is a palindrome when it reads the same forward and backward.
+    For example, 121 is a palindrome while 123 is not.
+
+
+ --------------------------------------------------------------------------
+*/
+function isPalindrome(int $number): bool
+{
+    if ($number < 0) {
+        return false;
+    }
+
+    $string = (string) $number;
+    $reversed = strrev($string);
+
+    return $reversed === $string;
+}
+
+
+test('easy is palindrome', function ($number, $result) {
+    expect(isPalindrome($number))->toBe($result);
+})
+    ->with([
+        [121, true],
+        [-121, false],
+        [10, false],
+    ]);
